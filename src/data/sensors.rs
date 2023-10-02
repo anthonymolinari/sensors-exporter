d91e85b2069c053990f55344292039676ca78220
anthonymolinari <molinaria@sonoma.edu>
2023-10-02 16:28:05 -0700

    fixed typo

--- src/data/sensors.rs
+++ src/data/sensors.rs
@@ -14,7 +14,7 @@ pub fn get_sensor_data() -> Result<String, Error> {
                         .replace(" ", "_")
                         .replace("-", "_")
                         .replace("+", "");
-                    body.push_str(format!("# TYPE {} gague \n", metrics_label).as_str());
+                    body.push_str(format!("# TYPE {} gauge \n", metrics_label).as_str());
                     body.push_str(format!("# HELP {} {:?} \n", metrics_label, sub_feature.kind().unwrap_or_default()).as_str());
                     body.push_str(format!("{} {}\n", metrics_label, value).as_str());
                 }
