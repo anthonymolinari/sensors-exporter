0c22b8836c9bc1d0374340462ad40e2c54bc00e7
anthonymolinari <molinaria@sonoma.edu>
2023-10-01 22:58:27 -0700

    rafactoring

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,17 @@
-use lm_sensors::prelude::*;
-use lm_sensors::errors::Error;
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+mod data;
+mod http;
 
-fn get_sensor_data() -> Result<String, Error> {
-    let sensors = lm_sensors::Initializer::default().initialize()?;
-    let mut body = String::new();
-
-    for chip in sensors.chip_iter(None) {
-        for feature in chip.feature_iter() {
-            let name = feature.name().transpose()?.unwrap_or("N/A");
-            for sub_feature in feature.sub_feature_iter() {
-                if let Ok(value) = sub_feature.raw_value() {
-                    body.push_str(format!("# TYPE {:?} \n", sub_feature.kind().unwrap_or_default()).as_str());
-                    let metrics_label = format!("{}_{}_{}_{}", chip, name, feature, sub_feature)
-                        .replace(" ", "_")
-                        .replace("-", "_")
-                        .replace("+", "");
-                    body.push_str(format!("{} {}\n", metrics_label, value).as_str());
-                }
-            }
-        }
-    }
-    Ok(body)
-}
-
-#[get("/metrics")]
-async fn metrics() -> impl Responder {
-    match get_sensor_data() {
-        Ok(data) => HttpResponse::Ok().body(data),
-        Err(_) => HttpResponse::InternalServerError().body("error"),
-    }
-}
-
-#[get("/health")]
-async fn health() -> impl Responder {
-    "ok"
-}
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // add logger
     HttpServer::new(|| {
         App::new()
-            .service(health)
-            .service(metrics)
+            .service(http::router::health)
+            .service(http::router::metrics)
     })
     .bind(("0.0.0.0", 8282))?
     .run()
     .await
 }
-
