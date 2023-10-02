caefdee599c232ec170ce0492bc381ddb87671e5
anthonymolinari <molinaria@sonoma.edu>
2023-10-01 22:00:09 -0700

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,16 @@ fn get_sensor_data() -> Result<String, Error> {
             let name = feature.name().transpose()?.unwrap_or("N/A");
             for sub_feature in feature.sub_feature_iter() {
                 if let Ok(value) = sub_feature.raw_value() {
-                    body.push_str(
-                        format!("#\n"
-                        ).as_str()
-                    );
-                    body.push_str(
-                        format!("{}_{}_{}_{} {}\n", 
-                                chip,
-                                name,
-                                feature,
-                                sub_feature,
-                                value
-                        ).as_str()
-                    );
+                    let metrics_label = format!("{}_{}_{}_{}", chip, name, feature, sub_feature)
+                        .replace(" ", "_")
+                        .replace("-", "_")
+                        .replace("+", "");
+                    body.push_str(format!("{} {}\n", metrics_label, value).as_str());
                 }
             }
         }
     }
-    Ok(
-        body.to_string()
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("-", "_")
-            .replace("+", "")
-    )
+    Ok(body)
 }
 
 #[get("/metrics")]
